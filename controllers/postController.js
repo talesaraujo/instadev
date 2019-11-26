07fe5bf8e0546243da8bc99f5672caf2d1213ff3
talesaraujo <talesaraujodossantos@gmail.com>
2019-11-26 10:42:45 -0300

    Added edit posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -17,6 +17,9 @@ const fetchPost = async (req, res) => {
     try {
         const post = await Post.findById(req.params.postId).populate('user'); // Same! :p
 
+        if (!post) {
+            return res.status(404).send({ error: 'Post not found' });
+        }
         return res.send({ post });
     }
     catch(err) {
@@ -38,7 +41,17 @@ const createPost = async (req, res) => {
 
 
 const editPost = async (req, res) => {
-    return res.send({ user: req.userId });
+    try {
+        const newPost = await Post.findByIdAndUpdate(req.params.postId, req.body, { new: true });
+
+        if (!newPost) {
+            return res.status(404).send({ error: 'Post not found' });
+        }
+        return res.send({ newPost });
+    }
+    catch (err) {
+        return res.status(400).send({ error: 'Error while editing post' });
+    }
 }
 
 
