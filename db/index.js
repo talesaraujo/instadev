f0421efadcf3ba6d7a866db7aa11474cc614658b
talesaraujo <talesaraujodossantos@gmail.com>
2019-12-02 21:11:49 -0300

    Rollback changes

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/instadev',
+mongoose.connect('mongodb://localhost:27017/instadev',
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-//const multer = require('multer');
-//const multerConfig = require("../config/multer");
-
 const postController = require("../controllers/postController");
 const authMiddleware = require('../middlewares/auth');
 
